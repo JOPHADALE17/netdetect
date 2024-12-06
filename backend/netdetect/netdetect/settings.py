f5d4a6b3491cd82684f6e603527a2afefd0245b5
Jophadale Carl Velasco <nightshadearcane172@gmail.com>
2024-12-06 18:50:46 +0800

    Update settings.py

--- backend/netdetect/netdetect/settings.py
+++ backend/netdetect/netdetect/settings.py
@@ -119,8 +119,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jophadalecarlvelasco94@gmail.com'
-EMAIL_HOST_PASSWORD = 'wvlx mmfr vlxv ybjx'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # Password validation
